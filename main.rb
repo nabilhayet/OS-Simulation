84491ad9da9076be3aa1969065760d5c8f4f1b3e
nabilhayet <nabil4457@yahoo.com>
2024-07-16 12:47:42 -0400

    added cpu, array for processes and ready queue

--- main.rb
+++ main.rb
@@ -2,15 +2,23 @@ require './Pid.rb'
 puts "Please enter your name"
 name = gets.chomp
 puts "Hello, #{name}! I'm Ruby!"
+puts "------------------------------"
 $pid = 0
 $cpu = 0
 $process_array = Array.new
+$parent_child_process = Hash.new 
+$ready_queue = Array.new 
 
 
 def CreateProcess
   @process =  Pid.new($pid)
   $process_array.push(@process)
- 
+  if($ready_queue.length == 0)
+    $ready_queue.push(@process.pid)
+  end 
+  if($cpu == 0 && $ready_queue.length > 0)
+    $cpu = $ready_queue[-1]
+  end
 end 
 
 def FindProcessId
@@ -18,11 +26,28 @@ def FindProcessId
 end 
 
 def ShowAllProcess
-  puts $process_array[0].pid
+  puts "The processess are #{$process_array[0]}"
 end 
 
 CreateProcess()
 FindProcessId()
 ShowAllProcess()
+puts "----------------------"
+puts "The process using the cpu #{$cpu}"
+puts "The processes waitting in ready queue to use cpu are #{$ready_queue}"
+
+puts "Please enter your input"
+input = gets.chomp
+
+if(input == 'A')
+  puts $cpu
+  $parent_child_process["#{$cpu}"] = []
+else
+  $parent_child_process["#{$cpu}"].push(@process.FindProcessId)
+end 
+
+puts $parent_child_process
+
+
 
 
