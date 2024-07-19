644e107db1119b4ba28a05b278b33275486f2d56
nabilhayet <nabil4457@yahoo.com>
2024-07-19 12:52:38 -0400

    added input options for user

--- main.rb
+++ main.rb
@@ -1,7 +1,4 @@
 require './Pid.rb'
-puts "Please enter your name"
-name = gets.chomp
-puts "Hello, #{name}! I'm Ruby!"
 puts "------------------------------"
 $pid = 0
 $cpu = 0
@@ -9,16 +6,31 @@ $process_array = Array.new
 $parent_child_process = Hash.new 
 $ready_queue = Array.new 
 
-
 def CreateProcess
   @process =  Pid.new($pid)
   $process_array.push(@process)
+  $parent_child_process[$cpu] = []
+  MemoryUtilization()
+end 
+
+def MemoryUtilization
   if($ready_queue.length == 0)
     $ready_queue.push(@process.pid)
   end 
   if($cpu == 0 && $ready_queue.length > 0)
     $cpu = $ready_queue[-1]
   end
+end 
+
+def ProcessStatus
+  puts "----------------------"
+  puts "The process using the cpu #{$cpu}"
+  puts "The processes waitting in ready queue to use cpu are #{$ready_queue}"
+end 
+
+def ParentChildProcess
+  ($parent_child_process[$cpu] ||= []) << @process.findObject
+  puts $parent_child_process
 end 
 
 def FindProcessId
@@ -29,24 +41,26 @@ def ShowAllProcess
   puts "The processess are #{$process_array[0]}"
 end 
 
-CreateProcess()
-FindProcessId()
-ShowAllProcess()
-puts "----------------------"
-puts "The process using the cpu #{$cpu}"
-puts "The processes waitting in ready queue to use cpu are #{$ready_queue}"
 
 puts "Please enter your input"
 input = gets.chomp
 
-if(input == 'A')
-  puts $cpu
-  $parent_child_process[$cpu] = []
-else
-  ($parent_child_process[$cpu] ||= []) << @process.findObject
-end 
+while(input != 'Exit')
 
-puts $parent_child_process
+  if(input == 'A')
+    CreateProcess()
+  elsif(input == 'Q')
+    MemoryUtilization()
+  elsif(input == 'fork')
+    ParentChildProcess()
+  elsif(input == 'S r')
+    ProcessStatus()
+  end 
+  puts "Please enter your input"
+  input = gets.chomp
+end 
+   
+
 
 
 
