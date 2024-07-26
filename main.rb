a3c8841a0e0fff5efdef9fecd37f8f7340df930f
nabilhayet <nabil4457@yahoo.com>
2024-07-26 12:39:55 -0400

    added hard disk & IOQueue process

--- main.rb
+++ main.rb
@@ -57,27 +57,40 @@ end
 #         IOQueue: [2,6,7],
 #         processQueuefile: { 2: '2we.txt'}
 # }
+#  hash[:foo] ||= {}
+#  hash[:foo][:bar][:baz] = "hello"
 #
 
 def HardDiskRead
   puts "Hello World"
   $hard_disk_input[1] = Integer($hard_disk_input[1])
-#  hash[:foo] ||= {}
-#  hash[:foo][:bar][:baz] = "hello"
-  $hard_disk[$hard_disk_input[1]] ||= {} # {0 : {}}
-  $hard_disk[$hard_disk_input[1]]["process"] ||= {}
-  $hard_disk[$hard_disk_input[1]]["process"][$cpu] = $hard_disk_input[2]
+  if($hard_disk.has_key?($hard_disk_input[1])) # That means it hhas been read already
+    if(($hard_disk[$hard_disk_input[1]]["process"]).empty?)
+      $hard_disk[$hard_disk_input[1]]["process"][$cpu] = $hard_disk_input[2]
+     
+    else
+      $hard_disk[$hard_disk_input[1]]["IOQueue"] << $cpu
+      $hard_disk[$hard_disk_input[1]]["processqueuefile"][$cpu] = $hard_disk_input[2]
+      $cpu = 0
+      $cpu = $ready_queue[0]
+      $ready_queue.shift()
+    end 
+  else
+    $hard_disk[$hard_disk_input[1]] = {}
+    $hard_disk[$hard_disk_input[1]]["process"] = {}
+    $hard_disk[$hard_disk_input[1]]["IOQueue"] = []
+    $hard_disk[$hard_disk_input[1]]["processqueuefile"] = {}
+  end 
 
-  $hard_disk[$hard_disk_input[1]]["IOQueue"] ||= []
-  $hard_disk[$hard_disk_input[1]]["processqueuefile"] ||= {}
-  $hard_disk[$hard_disk_input[1]]["processqueuefile"][$cpu] = $hard_disk_input[2]
   puts $hard_disk
-
-  $cpu = 0
-  $cpu = $ready_queue[0]
-  $ready_queue.shift()
-
+ 
 end 
+ 
+ # $hard_disk[$hard_disk_input[1]] ||= {}
+ # $hard_disk[$hard_disk_input[1]]["process"] ||= {}
+ # $hard_disk[$hard_disk_input[1]]["IOQueue"] ||= []
+ # $hard_disk[$hard_disk_input[1]]["processqueuefile"] ||= {}
+ 
 
 def HardDiskStatus
   puts "The hard disk number being used now : "
@@ -87,6 +100,7 @@ def HardDiskStatus
     hard_disk_info.each do |h,d|
       puts "The #{h} is - #{d}"
       end 
+      puts "-------------------"
   end 
 end 
 
